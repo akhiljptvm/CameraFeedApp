be9ee7e2f4ac3457ed0a13155741f7e9a5276791
akhiljptvm <akhilakku008@gmail.com>
2024-09-23 17:30:26 +0530

    ServerChanges

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const WebSocket = require('ws');
 const fs = require('fs');
 
-const wss = new WebSocket.Server({ port: 8080 });
-let SampleText = "Successfully Data Arrived......";
+const port = process.env.PORT || 8080; // Use assigned port from Render or fallback to 8080 for local testing
+const wss = new WebSocket.Server({ port });
+// let SampleText = "Successfully Data Arrived......";
 
 
 wss.on('connection', (ws) => {
@@ -16,8 +17,8 @@ wss.on('connection', (ws) => {
       // Broadcast the received binary data to all connected clients
       wss.clients.forEach((client) => {
         if (client !== ws && client.readyState === WebSocket.OPEN) {
-          // client.send(message); // Sending the binary data to other clients
-          client.send(SampleText);
+          client.send(message); // Sending the binary data to other clients
+          // client.send(SampleText);
         }
       });
     } else {
@@ -26,11 +27,11 @@ wss.on('connection', (ws) => {
   });
 
 
-  
+
 
   ws.on('close', () => {
     console.log('Client disconnected.');
   });
 });
 
-console.log('WebSocket server is running on ws://localhost:8080');
+console.log('WebSocket server is running on ${port}');
